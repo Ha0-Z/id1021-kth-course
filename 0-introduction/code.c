964f2b199de09dc47ccfd399659bd2d78b85e53b
Ha0-Z <inbox@haozhang.se>
2025-01-14 22:39:17 +0100

    Inplement code and result for each run for array-c

--- 0-introduction/code.c
+++ 0-introduction/code.c
@@ -3,16 +3,213 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <time.h>
-long nano_seconds(struct timespec *t_start, struct timespec *t_stop) {
-    return (t_stop->tv_nsec- t_start->tv_nsec) +
-        (t_stop->tv_sec- t_start->tv_sec)*1000000000;
+
+#include <limits.h>
+
+long nano_seconds(struct timespec *t_start, struct timespec *t_stop)
+{
+    return (t_stop->tv_nsec - t_start->tv_nsec) +
+           (t_stop->tv_sec - t_start->tv_sec) * 1000000000;
 }
-int main() {
+
+// Approach 1
+// int main() {
+//     struct timespec t_start, t_stop;
+//     for(int i = 0; i < 10; i++) {
+//         clock_gettime(CLOCK_MONOTONIC, &t_start);
+//         clock_gettime(CLOCK_MONOTONIC, &t_stop);
+//         long wall = nano_seconds(&t_start, &t_stop);
+//         printf("%ld ns\n", wall);
+//     }
+// }
+
+// Approach 2
+
+// int main()
+// {
+//     struct timespec t_start, t_stop;
+//     int array[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+//     int sum = 0;
+//     clock_gettime(CLOCK_MONOTONIC, &t_start);
+//     for (int i = 0; i < 1000; i++)
+//     {
+//         sum += array[rand() % 10];
+//     }
+//     clock_gettime(CLOCK_MONOTONIC, &t_stop);
+//     long wall = nano_seconds(&t_start, &t_stop);
+//     printf("%ld ns\n", wall / 1000);
+// }
+
+long bench(int n, int loop)
+{
     struct timespec t_start, t_stop;
-    for(int i = 0; i < 10; i++) {
-        clock_gettime(CLOCK_MONOTONIC, &t_start);
-        clock_gettime(CLOCK_MONOTONIC, &t_stop);
-        long wall = nano_seconds(&t_start, &t_stop);
-        printf("%ld ns\n", wall);
+    int *array = (int *)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++)
+        array[i] = i;
+    int *indx = (int *)malloc(loop * sizeof(int));
+    for (int i = 0; i < loop; i++)
+        indx[i] = rand() % n;
+    int sum = 0;
+    clock_gettime(CLOCK_MONOTONIC, &t_start);
+    for (int i = 0; i < loop; i++)
+        sum += array[indx[i]];
+    clock_gettime(CLOCK_MONOTONIC, &t_stop);
+    long wall = nano_seconds(&t_start, &t_stop);
+    return wall;
+}
+
+// Approach 3
+
+// int main()
+// {
+//     for (int i = 0; i < 10; i++)
+//     {
+//         long wall = bench(1000, 3);
+//         printf("time : %ld ns\n", wall);
+//     }
+// }
+
+// Approach 4
+
+// int main(int argc, char *argv[])
+// {
+//     int n = 1000;
+//     int loop = 1000;
+//     int k = 10;
+//     long min = LONG_MAX;
+//     long max = 0;
+//     long total = 0;
+//     for (int i = 0; i < k; i++)
+//     {
+//         long wall = bench(n, loop);
+//         if (wall < min)
+//             min = wall;
+//         if (wall > max)
+//             max = wall;
+//         total += wall;
+//     }
+//     printf("maximum time: %0.2f ns/operation \n", (double)max / loop);
+//     printf("minimum time: %0.2f ns/operation \n", (double)min / loop);
+//     printf("average time: %0.2f ns/operation \n", (((double)total) / loop) / k);
+// }
+
+// Approach 5
+
+// int main(int argc, char *argv[])
+// {
+//     int sizes[] = {1000, 2000, 4000, 8000, 16000, 32000};
+//     int k = 10;
+//     int loop = 1000;
+//     for (int i = 0; i < 6; i++)
+//     {
+//         int n = sizes[i];
+//         long min = LONG_MAX;
+//         for (int i = 0; i < k; i++)
+//         {
+//             long wall = bench(n, loop);
+//             if (wall < min)
+//                 min = wall;
+//         }
+//         printf("%d %0.2f ns\n", n, (double)min / loop);
+//     }
+// }
+
+long search(int n, int loop)
+{
+    struct timespec t_start, t_stop;
+    int *array = (int *)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++)
+        array[i] = rand() % (n * 2);
+    int *keys = (int *)malloc(loop * sizeof(int));
+    for (int i = 0; i < loop; i++)
+        keys[i] = rand() % (n * 2);
+    int sum = 0;
+    clock_gettime(CLOCK_MONOTONIC, &t_start);
+    for (int i = 0; i < loop; i++)
+    {
+        int key = keys[i];
+        for (int j = 0; j < n; j++)
+        {
+            if (key == array[j])
+            {
+                sum++;
+                break;
+            }
+        }
+    }
+    clock_gettime(CLOCK_MONOTONIC, &t_stop);
+    long wall = nano_seconds(&t_start, &t_stop);
+    return wall;
+}
+
+// Search for an item
+
+// int main(int argc, char *argv[])
+// {
+//     int sizes[] = {1000, 2000, 4000, 8000, 16000, 32000};
+//     int k = 10;
+//     int loop = 1000;
+//     for (int i = 0; i < 6; i++)
+//     {
+//         int n = sizes[i];
+//         long min = LONG_MAX;
+//         for (int i = 0; i < k; i++)
+//         {
+//             long wall = search(n, loop);
+//             if (wall < min)
+//                 min = wall;
+//         }
+//         printf("%d %0.2f ns\n", n, (double)min / loop);
+//     }
+// }
+
+long duplicates(int n)
+{
+    struct timespec t_start, t_stop;
+    int *array_a = (int *)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++){
+        array_a[i] = rand() % (n * 2);
+    }
+    int *array_b = (int *)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        array_b[i] = rand() % (n * 2);
+    }
+    int sum = 0;
+    clock_gettime(CLOCK_MONOTONIC, &t_start);
+    for (int i = 0; i < n; i++)
+    {
+        int key = array_a[i];
+        for (int j = 0; j < n; j++)
+        {
+            if (key == array_b[j])
+            {
+                sum++;
+                break;
+            }
+        }
+    }
+    clock_gettime(CLOCK_MONOTONIC, &t_stop);
+    long wall = nano_seconds(&t_start, &t_stop);
+    return wall;
+}
+
+// Search for dublicates
+
+int main(int argc, char *argv[])
+{
+    int sizes[] = {1000, 2000, 4000, 8000, 16000, 32000};
+    int k = 10;
+    int loop = 1000;
+    for (int i = 0; i < 6; i++)
+    {
+        int n = sizes[i];
+        long min = LONG_MAX;
+        for (int i = 0; i < k; i++)
+        {
+            long wall = duplicates(n);
+            if (wall < min)
+                min = wall;
+        }
+        printf("%d %0.2f ns\n", n, (double)min / loop);
     }
 }
--- 0-introduction/text.md
+++ 0-introduction/text.md
@@ -4,15 +4,86 @@
 
 ## Method
 
-## Code review
+## The clock- Random access
+The code of clock which the array-c.pdf have provided have a few interellged procedure which determine the time in ns where the procedure take to run.
+Since it take some clockcyckle to record the current time with procedure **clock_gettime(CLOCK_MONOTONIC, &t_stop)**, there fore it will take some time before it ends.
 
-## Result
+#### Result of running the clock
+173 ns
+61 ns
+60 ns
+58 ns
+56 ns
+59 ns
+59 ns
+59 ns
+59 ns
+60 ns
 
-### Random access
+We can ignore the results at the first since the code where not cashed in the cpu cashe at the beginning.
+
+#### Solution
+Since the number of clockcyle to run same machine code is always the same, but it varies on diffrerent processor/ clockspeed of the processor. One solution is we can get the time to run the **clock_gettime()** procedure by record itself one. After that we can record the code we are intressed in and remove the time takes by running **clock_gettime()** simply subtract the value we recorded itself.
+
+### Approch 2 of running the clock by running 1000 times then devide by 1000
+In this approach all time takes were sumed together then devide by 1000, this approch is good in way of reduce the influence of time takes to record the current time by 1000 time, reduce to 11 promile of the original time which can be ignored. It also be able to record the arvage time takes to run the code.
 
 
-### Search
+### Approach 3
+#### Result
+time : 283 ns
+time : 91 ns
+time : 75 ns
+time : 69 ns
+time : 67 ns
+time : 67 ns
+time : 65 ns
+time : 67 ns
+time : 67 ns
+time : 68 ns
 
-### Dublicates
+### Approach 4
+#### Result
+maximum time: 4.52 ns/operation 
+minimum time: 4.16 ns/operation 
+average time: 4.21 ns/operation 
+
+### Approach 5
+#### Result
+1000 5.53 ns
+2000 5.52 ns
+4000 5.52 ns
+8000 5.54 ns
+16000 5.55 ns
+32000 5.57 ns
+
+
+### Optimization
+Undone
+
+
+## Search for an item
+#### Result
+1000 355.87 ns
+2000 690.61 ns
+4000 1401.87 ns
+8000 2719.43 ns
+16000 5622.37 ns
+32000 11156.55 ns
+#### Analisis
+n^n since you need to go through all element
+
+
+## Search for duplicates
+#### Result
+1000 762.92 ns
+2000 1399.13 ns
+4000 5602.12 ns
+8000 21749.18 ns
+16000 89383.37 ns
+32000 358672.26 ns
+#### 
+
+
 
 ## Discussion
